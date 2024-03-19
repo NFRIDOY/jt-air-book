511b02462321969cf948c4cbf68fd8f1a4c5e65e
NF RIDOY <nfridoy@gmail.com>
2024-03-19 15:44:32 +0600

    [UI] (navbar)

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import BannerImg from "../../assets/banner.png"
+
+export default function Banner() {
+    return (
+        <div className="-z-50">
+            <div className=" ">
+                <img src={BannerImg} alt="" />
+            </div>
+        </div>
+    )
+}
